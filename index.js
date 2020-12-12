236f4053e5d10fc2faab12b70de594f7575c7b79
Julia Palacios <moreparaya@gmail.com>
2020-12-12 18:43:51 +0000

    Persistance implementation

--- index.js
+++ index.js
@@ -1,52 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var DataStore = require('nedb');
+const dbConnect = require('./db');
+const app = require ('./server.js');
 
 var port = (process.env.PORT || 3000);
-var BASE_API_PATH = "/api/v1";
-var DB_FILE_NAME = __dirname + "/contacts.json";
 
 console.log("Starting API server..." +port);
 
-var app = express();
-app.use(bodyParser.json());
-
-var db = new DataStore({
-    filename: DB_FILE_NAME,
-    autoload:true
-});
-
-app.get("/", (req, res) => {
-    res.send("<html><body><h1>My server</h1></body></html>");
-});
-
-app.get(BASE_API_PATH + "/contacts", (req,res) => {
-    console.log(Date() + " - GET /contacts");
-    db.find({}, (err, contacts) => {
-        if(err){
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
-        }else{
-            res.send(contacts.map((contact) => {
-                delete contact.__id;
-                return contact;
-            }));
-        }
-    });
-});
-
-app.post(BASE_API_PATH + "/contacts",(req, res) => {
-    console.log(Date() + " - POST /contacts");
-    var contact = req.body;
-    db.insert(contact, (err) => {
-        if (err){
-            console.log(Date() + "-" + err);
-            res.sendStatus(500);
-        } else {
-            res.sendStatus(201);
-        }
-    });
-});
+dbConnect().then(
+    ()=>{
+       app.listen(port);
+       console.log("Server ready!"); 
+    },
+    err => {
+        console.log("Connection error: ");
+    }
+)
 
 app.listen(port);
 
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const Contact = require ('./contacts');
+
+var BASE_API_PATH = "/api/v1";
+
+var app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("<html><body><h1>My server</h1></body></html>");
+});
+
+app.get(BASE_API_PATH + "/contacts", (req,res) => {
+    console.log(Date() + " - GET /contacts");
+    
+    Contact.find({}, (err, contacts) => {
+        if(err){
+            console.log(Date() + "-" + err);
+            res.sendStatus(500);
+        }else{
+            res.send(contacts.map((contact) => {
+                return contacts.cleanup();
+            }));
+        }
+    });
+});
+
+app.post(BASE_API_PATH + "/contacts",(req, res) => {
+    console.log(Date() + " - POST /contacts");
+    var contact = req.body;
+    
+    Contact.create(contact, (err) => {
+        if (err){
+            console.log(Date() + "-" + err);
+            res.sendStatus(500);
+        } else {
+            res.sendStatus(201);
+        }
+    });
+});
